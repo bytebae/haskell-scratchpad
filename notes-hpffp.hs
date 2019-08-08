2e8115a41e2cd63d48b75ca6ff4937e1a5c1b836
ceod <phil0@protonmail.com>
2019-08-08 19:52:48 +0530

    completes the chapter on folding lists

--- notes-hpffp.hs
+++ notes-hpffp.hs
@@ -862,4 +862,123 @@ avgDb db = fromIntegral (sumDb db) / (fromIntegral (length $ filterDbNumber db))
 --
 -- foldr f z xs = foldl (flip f) z (reverse xs)
 --
+---
 
+---
+-- NOTE: Observe this recursive function that GENERATES the fib sequence
+fibs = 1 : scanl (+) 1 fibs -- !!!
+
+-- Return the nth fib number with the above function
+
+fibN n = fibs !! n -- this is so good
+
+---
+-- pg373 q3
+-- Try to write the factorial function from Recursion as a scan.
+-- You’ll want scanl again, and your start value will be 1. Warning:
+-- this will also generate an infinite list, so you may want to pass it
+-- through a take function or similar.
+
+factScanl :: Int -> Integer
+factScanl n = (scanl (*) 1 [1..])!! n --1 : scanl (*) [1..]
+
+-- pg375
+myAndFold :: [Bool] -> Bool
+myAndFold = foldr (&&) True
+
+myOrFold :: [Bool] -> Bool
+myOrFold = foldr (||) False
+
+myAnyFold :: (a -> Bool) -> [a] -> Bool
+-- myAnyFold f x = myOrFold $ map f x  -- not pointfree
+myAnyFold = \f x -> myOrFold $ map f x -- pointfree version... can be improved?
+
+-- Write two versions of myElem. One version should use folding
+-- and the other should use any.
+-- myElem :: Eq a => a -> [a] -> Bool
+-- Prelude> myElem 1 [1..10]
+-- True
+-- Prelude> myElem 1 [2..10]
+-- False
+
+myElemFold :: Eq a => a -> [a] -> Bool
+myElemFold k l = myAnyFold (\x -> x == k) l
+
+-- Implement myReverse, don’t worry about trying to make it
+-- lazy.
+-- myReverse :: [a] -> [a]
+-- myReverse = undefined
+-- Prelude> myReverse "blah"
+-- "halb"
+-- Prelude> myReverse [1..5]
+-- [5,4,3,2,1]
+
+myReverse :: [a] -> [a]
+myReverse []      = []
+myReverse (x:xs)  = (myReverse xs) ++ [x]
+
+-- 5. Write myMap in terms of foldr. It should have the same behavior
+-- as the built-in map.
+-- myMap :: (a -> b) -> [a] -> [b]
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f []      = []
+-- myMap f (x:xs) = [f x] ++ (myMap f xs) -- construct same operations with foldr
+myMap f (x:xs)  = foldr (++) [f x] [myMap f xs] -- expected expression
+
+-- Write myFilter in terms of foldr. It should have the same behav-
+-- ior as the built-in filter.
+-- myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f [] = []
+myFilter f (x: xs)
+    | f x == True = [x] ++ myFilter f xs
+    | otherwise   = myFilter f xs
+
+-- 7. squish flattens a list of lists into a list
+-- squish :: [[a]] -> [a]
+squish :: [[a]] -> [a]
+squish [] = []
+squish (x:xs) = x ++ (squish xs)
+
+-- 8. squishMap maps a function over a list and concatenates the re-
+-- sults.
+-- squishMap :: (a -> [b]) -> [a] -> [b]
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f []      = []
+squishMap f (x:xx)  = (f x) ++ (squishMap f xx)
+
+-- 9. squishAgain flattens a list of lists into a list. This time re-use the
+-- squishMap function.
+-- squishAgain :: [[a]] -> [a]
+
+squishAgain :: [[a]] -> [a]
+squishAgain l = squishMap (concat) [l]    -- Why [l]??
+
+-- 10. myMaximumBy takes a comparison function and a list and returns
+-- the greatest element of the list based on the last value that the
+-- comparison returned GT for.
+-- myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+
+myMaximumBy :: (Eq a, Ord a) => (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f (x:xs)
+    | xs == []              = x
+    | f x (head xs) == LT   = myMaximumBy f xs
+    | otherwise             = myMaximumBy f (x: (tail xs))
+
+
+-- 11. myMinimumBy takes a comparison function and a list and returns
+-- the least element of the list based on the last value that the
+-- comparison returned LT for.
+-- myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+
+myMinimumBy :: (Eq a, Ord a) => (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f (x:xs)
+  | xs == []              = x
+  | f x (head xs) == GT   = myMinimumBy f xs
+  | otherwise             = myMinimumBy f (x: (tail xs))
+
+-- SELF: write a function that maps a function over an iterable and returns [(result, enum)]
+myEnumMap :: (a -> b) -> [a] -> [(b, Integer)]
+myEnumMap = undefined
